b3653761cad29724509666ea262f51af7e6ffddf
wangwu <1065908163@qq.com>
2020-02-29 20:02:07 +0800

    标题ci3.yml

--- src/App.js
+++ src/App.js
@@ -4,13 +4,12 @@ import './App.css';
 import Game from './components/Game';
 
 function App() {
+  document.title = "井田棋";
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <Game />
       </header>
     </div>
